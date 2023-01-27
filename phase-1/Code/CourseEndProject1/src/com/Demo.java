8fe8456f4e58bb0cdc8427a377c6f71909d9fd49
David Vargas <dvargasfr@gmail.com>
2023-01-27 11:44:53 +0000

    course-end project 1 basic features
    Signed-off-by: David Vargas <dvargasfr@gmail.com>

---
+++ phase-1/Code/CourseEndProject1/src/com/Demo.java
@@ -0,0 +1,132 @@
+package com;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Demo {
+	
+	static Scanner scanner;
+	static boolean exitMain = false;
+	static boolean exitFileOps = false;
+	
+	public static void main(String[] args) {
+		
+		// WELCOME SCREEN
+		// app name and developer details
+		// display options for user interface
+		// features to accept the user input to select one of the options listed
+		
+		// OPTION 1
+		//		current file names in ascending order
+		// OPTION 2
+		//		details of the user interface
+		//		add file (ignore case sensitive)
+		//		delete file (with case sensitive. return msg if not found)
+		//		search file (with case sensitive, display result of success/unsuccess operation)
+		//		go back to main context
+		// OPTION 3
+		//		close the app
+		
+		do {
+			DisplayMainMenu();
+		}while(!exitMain);    
+	}
+	
+	public static void DisplayMainMenu() {
+		System.out.println("\nType the number of the action to run:");
+		System.out.println("1- List files");
+		System.out.println("2- File operations");
+		System.out.println("3- Exit");
+		System.out.print("> ");
+		scanner = new Scanner(System.in);
+		String input = scanner.nextLine();
+		switch(Integer.parseInt(input)) {
+		case 1:
+			ListFiles();
+			break;
+		case 2:
+			System.out.println("Chose 2");
+			do {
+				FileOps();
+			}while(!exitFileOps);
+			break;
+		case 3:
+			scanner.close();
+			exitMain = true;
+			break;
+		}
+	}
+	
+	public static void FileOps() {
+		System.out.println("\nType the number of the file operation to run");
+		System.out.println("1- add file");
+		System.out.println("2- delete file");
+		System.out.println("3- search file");
+		System.out.println("4- go back to main menu");
+		System.out.print("> ");
+		String input = scanner.nextLine();
+		switch(Integer.parseInt(input)) {
+		case 1:
+			System.out.println("Please type the file name to add:");
+			String inputFileName = scanner.nextLine();
+			CreateFile(inputFileName);
+			break;
+		case 2:
+			System.out.println("Please type the file name to delete:");
+			String deleteFileName = scanner.nextLine();
+			DeleteFile(deleteFileName);
+			break;
+		case 3:
+			System.out.println("Please type the file name to search:");
+			String searchFileName = scanner.nextLine();
+			SearchFile(searchFileName);
+			break;
+		case 4:
+			exitFileOps = true;
+			break;
+		}
+	}
+	
+	public static void CreateFile(String fileName) {
+		File file = new File("resources/"+fileName);
+		try {   
+			if(file.createNewFile()){  
+				System.out.println("file created "+file.getCanonicalPath());  
+			} else {  
+				System.out.println("File already exist at location: "+file.getCanonicalPath());  
+			}  
+		} catch (IOException e) {  
+			e.printStackTrace();  
+		}
+	}
+	
+	public static void ListFiles() {
+		String[] fileNames = GetFiles();
+		Arrays.sort(fileNames); 
+		for (String pathname : fileNames) {
+            System.out.println(pathname);
+        }
+	}
+	
+	public static String[] GetFiles() {
+		String[] pathnames;
+		File f = new File("resources");
+		pathnames = f.list();
+        return pathnames;
+	}
+	
+	public static void SearchFile(String fileName) {
+		String[] pathnames = GetFiles();
+		if(Arrays.asList(pathnames).contains(fileName)){
+			System.out.println("file found");
+		}else{
+			System.out.println("file not found");
+		}
+	}
+	
+	public static void DeleteFile(String fileName) {
+		System.out.println("Delete file "+fileName);
+	}
+}
