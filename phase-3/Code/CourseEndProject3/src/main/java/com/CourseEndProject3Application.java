034cce7b08d38ace797657f59a3967b88b8450de
David Vargas <dvargasfr@gmail.com>
2023-04-29 17:35:47 +0000

    admin view and add category
    Signed-off-by: David Vargas <dvargasfr@gmail.com>

--- phase-3/Code/CourseEndProject3/src/main/java/com/CourseEndProject3Application.java
+++ phase-3/Code/CourseEndProject3/src/main/java/com/CourseEndProject3Application.java
@@ -2,8 +2,12 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com")
+@EnableJpaRepositories(basePackages = "com.repository")
+@EntityScan(basePackages = "com.bean")
 public class CourseEndProject3Application {
 
 	public static void main(String[] args) {
---
+++ phase-3/Code/CourseEndProject3/src/main/java/com/service/CategoryService.java
@@ -0,0 +1,27 @@
+package com.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bean.Category;
+import com.repository.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	CategoryRepository categoryRepository;
+	
+	public String storeCategory(Category category) {
+		System.out.println("categoryService.storeCategory: "+category);
+		categoryRepository.save(category);
+		return "Category details stored";
+	}
+	
+	public List<Category> findAllCategory() {
+		System.out.println("findAll: "+categoryRepository.findAll());
+		return categoryRepository.findAll();
+	}
+}
